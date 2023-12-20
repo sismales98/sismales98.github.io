1b0e2cbcca51ced7d4e1ae796f04f2855d098880
sismales98 <152348798+sismales98@users.noreply.github.com>
2023-12-19 21:57:39 -0500

    change number test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 function openWhatsapp(){
-    var numero = "+573205181852";
+    // var numero = "+573205181852";
+    var numero = "+15550801863";
     var menssage = "Hola quiero hacer un vuelo en parapente";
     var menssageCo= encodeURIComponent(menssage);
     var linkWhatsapp = 'https://wa.me/' + numero + "?text=" + menssageCo;
