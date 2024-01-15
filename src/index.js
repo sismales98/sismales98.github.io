7b5c93f64c9f2edd0481bcaade2ae1652a142bab
sismales98 <152348798+sismales98@users.noreply.github.com>
2024-01-14 20:43:02 -0500

    animation

--- src/index.js
+++ src/index.js
@@ -10,5 +10,44 @@ function openWhatsapp(){
 }
 
 
+//Animation
 
 
+
+
+ 
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var toggleButton = document.getElementById("toggleButton");
+    var buttonChange = document.getElementById("button-change");
+
+    toggleButton.addEventListener("click", function () {
+      // Toggle entre las clases fa-angles-down y fa-angles-up
+      buttonChange.classList.toggle("fa-angles-up");
+      buttonChange.classList.toggle("fa-angles-down");
+     
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var toggleButton = document.querySelector(".navbar-toggler");
+  
+    // Cambiar la propiedad 'animation' al hacer clic en el botón
+    toggleButton.addEventListener("click", function() {
+      // Asegúrate de que toggleButton tiene la clase 'despliegueB' antes de cambiarla
+      if (toggleButton.classList.contains("despliegueB")) {
+        toggleButton.style.animation = "desplegueB 0.8s infinite";
+        // Asegúrate de cambiar la clase para futuras interacciones
+      
+        toggleButton.style.removeProperty("animation");
+        toggleButton.classList.remove("despliegueB");
+      } else {
+        // Restaura la propiedad 'animation' original
+        toggleButton.style.animation = "despliegueUp 0.8s infinite";
+        // Asegúrate de cambiar la clase para futuras interacciones
+        toggleButton.classList.add("despliegueB");
+      }
+    });
+  });
